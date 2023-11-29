44746709476ffd22c955f9fe4f71e0d4c8c1c603
ahmediosama <kaitestoon2005@gmail.com>
2023-11-29 14:27:42 -0800

    ko

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i % 15 == 0:
+        print("FizBuzz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    else:
+        print("{:d}".format(i),end=" ")
